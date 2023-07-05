0edd23c2112792c6c10ed2a6891cfd84f06a8996
John <jovijovi2019@hotmail.com>
2023-07-05 15:07:11 -0300

    Aceitando requisições de outros sites

--- server/src/index.ts
+++ server/src/index.ts
@@ -19,12 +19,7 @@ app.use(express.json());
 app.use(router);
 
 const server = http.createServer(app);
-const io = new Server(server, {
-  cors: {
-    origin: "http:localhost:5173",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-  },
-});
+const io = new Server(server);
 
 io.on("connection", async (socket) => {
   console.log(`user connected: ${socket.id}`);
